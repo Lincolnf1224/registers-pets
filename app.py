94037ebfd41ea5860013d2b96af9570085770df5
Lincolnf1224 <141071287+Lincolnf1224@users.noreply.github.com>
2023-11-11 19:26:04 -0300

    create app.py
    primeiro codigo do projeto

---
+++ app.py
@@ -0,0 +1,15 @@
+
+print('ola \n')
+
+from time import sleep
+sleep(2)
+
+print('[h]elp para ajuda                |\n'
+      '[r]egister para um novo registro |\n'
+      '[v]iew para ver seus dados       |\n'
+      '[a]lter para alterar seus dados  |')
+
+pergunta = str(input('o que deseja fazer?'))
+
+
+
