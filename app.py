8d943313d2c73abb84038bdbdfff3f7b624f55a3
Lincolnf1224 <141071287+Lincolnf1224@users.noreply.github.com>
2023-12-06 18:35:43 -0300

    resultado para [r]
    comando para colocar um dado no banco

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-
 print('ola \n')
 
 import pyodbc
 dados_conexao = (
                  'driver={MySQL ODBC 8.0 ANSI Driver};'
                 'server=localhost:3306;'
-                'database=cadastro;'
+                'database=petshop;'
                 'uid=root;'
                 'pwd=07112616LRAM#;'
                 'charset=utf8mb4;'
  )
 
 conexao = pyodbc.connect(dados_conexao)
+cursor = conexao.cursor()
 
 print('conexao bem sucedida'
       '\n')
 
 from time import sleep
-sleep(2)
-
-print('[h]elp para ajuda                |\n'
-      '[r]egister para um novo registro |\n'
-      '[v]iew para ver seus dados       |\n'
-      '[a]lter para alterar seus dados  |')
+sleep(0)
 
 pergunta = str(input('o que deseja fazer?'))
 
@@ -33,8 +28,26 @@ if pergunta == 'h':
       '[v]iew para ver seus dados       |\n'
       '[a]lter para alterar seus dados  |')
 
-#elif  pergunta == 'r':
-    #nome
+elif  pergunta == 'r':
+    # nota : fazer uma classe que execute r , execute v e execute a
+
+    especie = str(input('especie:')).upper()
+    nome = str(input('nome:'))
+    sexo = str(input('sexo:')).upper()
+    
+    criar = (f'''insert into pets (id , especie , nome , sexo) values(default ,'{especie}','{nome}', '{sexo}')''')
+    cursor.execute(criar)
+    cursor.commit()
+    print('sucesso')
+
+#else temporario
+else:
+    c = 'select * from pets'
+    cursor.execute(c)
+    resposta = cursor.fetchall()
+    print(resposta)
+
+
 
 
 
